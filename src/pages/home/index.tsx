5a48a5e347f2eca07d720fb6f7eca06cba2c7318
Gilmar Júnior <gilmarjunior@outlook.com>
2025-09-27 15:43:42 -0300

    20250927-1543

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, HStack } from "@chakra-ui/react";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import { useState } from "react";
 
 function Home() {
+  const [open, setOpen] = useState<boolean>(true);
+
   return (
     <Box
       bg={"natural.100"}
@@ -9,35 +14,64 @@ function Home() {
       position={"relative"}
       width={"100vw"}
     >
-      <Box
+      <HStack
         bg={"orange.500"}
         h={"56px"}
         position={"absolute"}
         w={"100%"}
       >
-        <Box>navbar</Box>
-      </Box>
+        <Button
+          _hover={{ bg: "orange.600" }}
+          bg={"orange.500"}
+          borderRadius={0}
+          display={{ md: "block", lg: "none" }}
+          h={"56px"}
+          w={"56px"}
+          onClick={() => setOpen(!open)}
+        >
+          <Icon
+            path={mdiMenu}
+            size={1}
+          />
+        </Button>
+      </HStack>
 
       <Box
+        bg={{ base: "natural.0", lg: "natural.100" }}
         bottom={0}
-        left={0}
+        boxShadow={{ base: "md", lg: "none" }}
+        left={{ base: open ? "0" : "-350px", lg: "0px" }}
         overflowY={"auto"}
         p={"16px"}
         position={"absolute"}
         top={"56px"}
+        transition={"left .5s ease"}
         w={"350px"}
+        zIndex={9000}
       >
         <Box>sidebar</Box>
       </Box>
 
       <Box
+        bg={"blackAlpha.500"}
         bottom={0}
-        left={"350px"}
+        left={0}
+        position={"absolute"}
+        right={{ base: open ? 0 : "100vw", lg: "100vw" }}
+        top={"56px"}
+        zIndex={8999}
+        onClick={() => setOpen(false)}
+      />
+
+      <Box
+        bottom={0}
+        left={{ base: "0px", lg: "350px" }}
         overflowY={"auto"}
         p={"16px"}
         position={"absolute"}
         right={0}
         top={"56px"}
+        transition={"left .5s ease"}
       >
         <Box
           bg={"natural.0"}
@@ -45,7 +79,11 @@ function Home() {
           boxShadow={"md"}
           p={"16px"}
         >
-          <Box>body</Box>
+          <Box>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur nobis incidunt non corrupti,
+            possimus suscipit rem illum autem molestias sequi dolores nesciunt molestiae temporibus vero debitis maiores
+            nostrum minima?
+          </Box>
         </Box>
       </Box>
     </Box>
